905d0c8a5b5d1ce0bf980fad661303c85d6e6bd5
shotaro-ooe <shoooe97@gmail.com>
2021-08-08 12:46:15 +0900

    initial commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by 大江祥太郎 on 2021/08/08.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        
+    }
+    
+}
+
