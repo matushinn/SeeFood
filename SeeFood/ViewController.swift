972a9ce6f835b7d0bed3cd170d4684a3d0b48716
shotaro-ooe <shoooe97@gmail.com>
2021-08-08 13:27:12 +0900

    hotdogの分析完了

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -19,12 +19,64 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         super.viewDidLoad()
         
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = false
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedimage = info[.originalImage] as? UIImage {
+            imageView.image = userPickedimage
+            
+            guard let ciimage = CIImage(image: userPickedimage) else{
+                fatalError("Could not convert UIImage into CIIMage")
+            }
+            
+            detect(image: ciimage)
+            
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image:CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process image.")
+            }
+            
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "HotDog!"
+                }else{
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch  {
+            print(error)
+
+        }
+        
+        
+        
+        
+        
+        
+    }
     @IBAction func cameraTapped(_ sender: Any) {
         
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
